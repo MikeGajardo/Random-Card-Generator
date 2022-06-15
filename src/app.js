0126001a7ea8cd06663b4715e1b128f376996757
MikeGajardo <mikegajardo@gmail.com>
2022-06-15 01:29:59 +0000

    random card

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+function randomNumber() {
+  let arrNum = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let randomIndex = Math.floor(Math.random() * arrNum.length);
+  return arrNum[randomIndex];
+}
+
+function randomSuit() {
+  let arrSuit = ["&spades;", "&hearts;", "&clubs;", "&diams;"];
+  let randomSuit = Math.floor(Math.random() * arrSuit.length);
+
+  if (randomSuit == 1 || randomSuit == 3) {
+    document.querySelector(".card-container").classList.add("color-red");
+  } else {
+    document.querySelector(".card-container").classList.add("color-black");
+  }
+  return arrSuit[randomSuit];
+}
+
+let suitValue = randomSuit();
+
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#random").innerHTML = randomNumber();
+  document.querySelector(".top").innerHTML = suitValue;
+  document.querySelector(".bottom").innerHTML = suitValue;
 };
