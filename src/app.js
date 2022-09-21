4bb6c49d927f6a44fe58552f04aa18393822702f
MikeGajardo <mikegajardo@gmail.com>
2022-09-21 00:34:38 +0000

    added randomizer button and logic

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//Function containing our array of numbers that produces a random outcome
 function randomNumber() {
   let arrNum = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
   let randomIndex = Math.floor(Math.random() * arrNum.length);
   return arrNum[randomIndex];
 }
 
+//Function containing our array of suits we use the dom to add the class to a random index
 function randomSuit() {
   let arrSuit = ["&spades;", "&hearts;", "&clubs;", "&diams;"];
   let randomSuit = Math.floor(Math.random() * arrSuit.length);
@@ -24,6 +26,14 @@ function randomSuit() {
 }
 
 let suitValue = randomSuit();
+
+const refreshButton = document.querySelector(".refresh-button");
+
+const refreshPage = () => {
+  window.location.reload();
+};
+
+refreshButton.addEventListener("click", refreshPage);
 
 window.onload = () => {
   //write your code here
